7fc8eb41933990e3d7f2a64cd83e24c3d926514d
Student <example@example.com>
2019-02-20 18:47:02 -0800

    Reads in the 3 triangle sides, reads them in, converts them to integer values, repeats them to the user, and adds them together.

--- Models/Triangle.cs
+++ Models/Triangle.cs
@@ -12,26 +12,18 @@ class Program
 {
   public static void Main()
   {
+    // Reads in the 3 variables and saves them as integers
     Console.WriteLine("-----------------------");
     Console.WriteLine("What is first side?");
     int UserSideOne = int.Parse(Console.ReadLine());
-    // string UserSideOne = Console.ReadLine();
     Console.WriteLine("What is the second side?");
-    string UserSideTwo = Console.ReadLine();
+    int UserSideTwo = int.Parse(Console.ReadLine());
     Console.WriteLine("What is the third side?");
-    string UserSideThree = Console.ReadLine();
+    int UserSideThree = int.Parse(Console.ReadLine());
+
+    // Ouputs the inputted sides
     Console.WriteLine("Your inputs for the triangle is " + UserSideOne + " and " + UserSideTwo + " and " + UserSideThree);
-    // int result = int.Parse(UserSideOne) + int.Parse(UserSideTwo) + int.Parse(UserSideThree);
-    // Console.WriteLine(result);
-
-    // if (UserSideOne) {
-      // if (UserSideOne == UserSideTwo && UserSideTwo == UserSideThree) {
-        // string quote = "It is an equilateral triangle";
-      // }
-
-    // Console.WriteLine(quote);
-
-
-    // }
+    int result = UserSideOne + UserSideTwo + UserSideThree;
+    Console.WriteLine(result);
   }
 }
